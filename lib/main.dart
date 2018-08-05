9e5c0456e9b3a7b57bac203e95053dcd91bbe55f
Nazif Abdullah <ahmadnazif@gmail.com>
2018-08-05 21:35:35 +0800

    changed "MaterialPageRoute.debugEnableFadingRoutes" to true

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'startscreen-list.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  MaterialPageRoute.debugEnableFadingRoutes = false;
+  MaterialPageRoute.debugEnableFadingRoutes = true;
 
   runApp(MaterialApp(
       title: "Nazif's App",
